ef0d5f4aad54e76a45a056affba1927a9d099be7
slague <stephanielague@gmail.com>
2017-03-27 10:16:11 -0600

    tags are saving, no show page for tags

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -49,7 +49,7 @@ class ArticlesController < ApplicationController
   private
 
   def article_params
-    params.require(:article).permit(:title, :body)
+    params.require(:article).permit(:title, :body, :tag_list)
   end
 
 
